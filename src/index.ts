abf5cd4d2facdfa677a70382a0a0eca2a3a5e6f0
battlesquid <25509915+Battlesquid@users.noreply.github.com>
2020-11-21 11:38:09 -0700

    archiver done, cleaning up

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,16 @@
 import env from "dotenv";
 env.config({ path: "../.env" });
-import { getYearBound, queryQuestions } from "./server/modules/queryPages";
-import express from "express";
 
-const app: express.Application = express();
+import { archiveCategory } from "./modules/queryPages";
+
+const categories: string[] = ["VRC", "VEXU", "VIQC", "VAIC-HS", "VAIC-U", "RADC", "Judging"];
 
 (async () => {
-    const year_bound = await getYearBound();
-    console.log(year_bound)
-    for (let year = 2018; year < year_bound; year++) {
-        const res = await queryQuestions("VRC", year);
-        console.log(res)
+
+    console.time("Archival Processing Time:");
+    for (const category of categories) {
+        console.log("===========", category, "===========")
+        await archiveCategory(category);
     }
-})()
-
-// const validateQuery = (category: string, keywords: string, wholeWord: string): boolean => {
-//     if (!(category || keywords || wholeWord)) return false;
-
-//     // console.log(category, keywords, wholeWord)
-
-//     const validCategory = ["VRC", "VEXU", "VIQC", "VAIC-HS", "VAIC-U", "RAD"].includes(category);
-//     const validKeywords = keywords.split(/[\+ ]/).filter(Boolean).length > 0;
-//     const validWholeWord = wholeWord === "0" || wholeWord === "1";
-//     return validCategory && validKeywords && validWholeWord;
-// }
-
-// app.get('/api/search', async (req, res) => {
-//     const { category, keywords, wholeWord } = req.query;
-//     const isValidQuery = validateQuery(category as string, keywords as string, wholeWord as string)
-
-//     if (isValidQuery) {
-//         console.log("valid query!")
-//         const qnas = await queryQuestions(category as string, keywords as string, wholeWord as string);
-//         res.send(qnas)
-//     } else {
-//         console.log("invalid query")
-//     }
-// })
-
-app.listen(process.env.PORT, () => console.log("Server Started."))
+    console.timeEnd("Archival Processing Time:");
+})()   
