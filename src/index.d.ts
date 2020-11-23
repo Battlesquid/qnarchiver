1353f5815a43c2ee966c6b73e7ff7be317365fa4
battlesquid <25509915+Battlesquid@users.noreply.github.com>
2020-11-23 15:54:11 -0700

    remove index typings

--- src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ArchiverOptions, QnaQuestion } from "./types"
-
-export class Archiver {
-    constructor(apiKey: string, options: ArchiverOptions)
-    processCategory(category: string, shouldReturn: boolean): Promise<QnaQuestion[] | undefined>
-}
