2e8d987299b782b7d2da9211f93ff595aafc1379
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2023-08-06 23:10:39 -0700

    refactor: rewrite default seasons constant

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -2,9 +2,10 @@ import { Season } from "../types";
 
 export const CURRENT_YEAR = new Date(Date.now()).getFullYear();
 
-export const DEFAULT_SEASONS: Season[] = [];
-for (let year = 2018; year <= CURRENT_YEAR; year++) {
-    DEFAULT_SEASONS.push(`${year}-${year + 1}`);
-}
+export const START_YEAR = 2018;
+
+export const DEFAULT_SEASONS: Season[] = Array(CURRENT_YEAR - START_YEAR + 1)
+    .fill(0)
+    .map<Season>((_, i) => `${START_YEAR + i}-${START_YEAR + i + 1}`);
 
 export const DEFAULT_PROGRAMS = ["VRC", "VEXU", "VIQRC", "Judging"] as const;
