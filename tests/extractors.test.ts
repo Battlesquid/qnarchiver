a5a2ba00fc4a5d6722ab712cfca138571cf47153
Battlesquid <esanoluwamayowa@gmail.com>
2025-03-02 21:19:12 -0700

    chore: add readonly test

--- tests/extractors.test.ts
+++ tests/extractors.test.ts
@@ -1,9 +1,10 @@
-import { extractPageCount, extractPageQuestions, extractQuestion } from "../src/modules/extractors";
+import { extractPageCount, extractPageQuestions, extractQuestion, extractReadOnly } from "../src/modules/extractors";
 import judging_home_page from "./data/judging_home_page";
 import vurc_home_page from "./data/vurc_home_page";
 import v5rc_home_page from "./data/v5rc_home_page";
 import v5rc_question_page from "./data/v5rc_question_page";
 import v5rc_question_page_json from "./data/v5rc_question_page_json";
+import v5rc_non_readonly_page from "./data/v5rc_non_readonly_page";
 
 describe("extractPageCount", () => {
     it("shall return the correct page number when the pagination element is present", () => {
@@ -41,3 +42,10 @@ describe("extractPageQuestions", () => {
         ]);
     });
 });
+
+describe("extractReadOnly", () => {
+    it("shall extract the readonly status from a Q&A page", () => {
+        expect(extractReadOnly(v5rc_home_page)).toBe(true);
+        expect(extractReadOnly(v5rc_non_readonly_page)).toBe(false);
+    })
+})
